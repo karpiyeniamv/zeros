7fea230f9d39519f357ad86f328195323e3fa3a2
Maryia Karpiyenia <karpiyenia.mv@gmail.com>
2018-10-01 09:51:09 +0300

    true res

--- src/index.js
+++ src/index.js
@@ -1,22 +1,18 @@
-module.exports = function getZerosCount(number) {
-    var step =  parseInt( Math.log(number) / Math.log(5));
-    var res = 0;
-    // for (var i=1; i<step+1; i++)
-    // {
-    //     res+=1/(Math.pow(5,i));
-    // }
-    var i=1;
-    //while (parseInt(number/(Math.pow(5,i)))>=2)
-    while (i<step)
+module.exports =  function getZerosCount(number) {
+    res = 0;
+    del=5;
+    k= number/del;
+    while (k>0.5)
     {
-        res+= 1/(Math.pow(5,i));
-        
-        i++;
-    }
-    res=res*10000000;
-    res=parseInt(res);
-    res=res/10000000;
-    fact = Math.round (number* (res));
-    return fact;
-}
+        // console.log( 'k='+k);
+        // console.log( 'del='+del);
+        // console.log( 'res='+res);
+        res+= parseInt(k);
+        del*=5;
+        k=number/del;
 
+
+
+    }
+    return res;
+};
